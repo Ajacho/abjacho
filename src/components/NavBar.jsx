5ba2f26e608193bb038cdbcfa43c3ca454f8a6a0
ajacho <belen.jacho18@gmail.com>
2025-07-09 20:33:58 -0700

    Changed navbar layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,45 +1,61 @@
+
 import { useState } from "react";
 import { Link } from "react-scroll";
-import { Menu, X } from "lucide-react";
+import { Github, Linkedin, Mail } from "lucide-react";
 import "../styles/NavBar.css";
 
 
 export const NavBar = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
+    const desktopNavItems = ["Home", "Experience", "Projects"];
+
+    const mobileNavItems = [
+        { 
+            icon: Github,
+            name: "GitHub", 
+            link: "https://github.com/Ajacho", 
+            target:"_blank", 
+        },
+        { 
+            icon: Linkedin,
+            name: "LinkedIn", 
+            link: "https://www.linkedin.com/in/abjacho/", 
+            target:"_blank", 
+        },
+        { 
+            icon : Mail, 
+            name: "Email", 
+            link: "mailto:belen.bjacho@gmail.com", 
+            target:"_blank", 
+        }
+    ]
 
     return (
-        <nav className="fixed top-0 left-0 z-50 w-full shadow-md bg-gradient-to-l from-[#063064] to-[ #0a192fd7]">
+        <nav className="fixed top-0 left-0 z-50 w-full shadow-md bg-gradient-to-l from-[#063064] to-[#0a192fd7]">
             <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-                <a href="#" className="text-2xl font-bold "> abjacho</a>
+                <a href="#" className="text-2xl font-bold hover:text-yellow-400 ">Andrea B Jacho</a>
 
                 {/* Desktop Nav */}
-                <ul className="hidden md:flex gap-6 ">
-                    {["Home", "Experience", "Projects"].map((item) => (
-
-                        <li key={item}>
-                            <Link to={item.toLowerCase()} smooth={true} duration={500} className="cursor-pointer hover:text-gray-900">{item}</Link>
+                <ul className="hidden md:flex gap-6 text-sm">
+                    {desktopNavItems.map((item) => (
+                        <li 
+                            key={item} 
+                            className="hover:text-yellow-400 transition-colors duration-300 text-base">
+                            <Link to={item.toLowerCase()} smooth={true} duration={500} className="cursor-pointer">{item}</Link>
                         </li>
                     ))}
+
                 </ul>
-
-                {/* Mobile Nav btn */}
-                <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
-                    {isOpen ? <X className="" size={30} /> : <Menu className="" size={30} />}
-                </button>
+                <ul className="text-white flex gap-4">
+                    {mobileNavItems.map((item) => (
+                        <li key={item.name} className="list-none hover:text-yellow-400 transition-colors duration-300">
+                            <a href={item.link} target={item.target} rel="noopener noreferrer">
+                                <item.icon className="inline-block w-6 h-6 mr-2" />
+                            </a>
+                        </li>
+                    ))}
+                </ul>              
             </div>
-
-                {/* Mobile Nav Menu*/}
-                {isOpen && (<div className="md:hidden">
-                    <ul className="flex flex-col gap-4 items-center">
-                        {["Home", "Experience", "Projects"].map((item) => (
-                            <li key={item}>
-                                <Link to={item.toLowerCase()} smooth={true} duration={1000} onClick={() => setIsOpen(false)} className="cursor-pointer hover:text-blue-900">{item}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                )}
         </nav>
     );
 };

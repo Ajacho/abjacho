99179137e9a73529e61560c08b6e36d0f446b021
ajacho <belen.jacho18@gmail.com>
2026-04-21 11:33:59 -0700

    Navbar done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,48 +29,52 @@ export const NavBar = () => {
 
   return (
     <section id="navbar">
-    <nav className="fixed top-0 left-0 z-50 w-full shadow-md bg-gradient-to-l from-[#063064] to-[#0a192fd7]">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-        <a href="#" className="text-2xl font-bold hover:text-yellow-400 text-lg">
-          Andrea B Jacho
-        </a>
+      <nav className="fixed top-0 left-0 z-50 w-full shadow-md bg-transparent backdrop-blur-lg stroke-black/80 text-yellow-400">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+          <a
+            href="#"
+            className="text-2xl font-bold hover:text-yellow-400 text-lg px-2 py-1 rounded-lg transition-colors duration-300"
+          >
+            Andrea B Jacho
+          </a>
 
-        {/* Desktop Nav */}
-        <ul className="hidden md:flex gap-6 text-sm">
-          {desktopNavItems.map((item) => (
-            <li
-              key={item}
-              className="hover:text-yellow-400 transition-colors duration-300 text-base"
-            >
-              <Link
-                to={item.toLowerCase()}
-                smooth={true}
-                duration={500}
-                className="cursor-pointer"
+          {/* Desktop Nav */}
+          <ul className="hidden md:flex gap-6 text-sm">
+            {desktopNavItems.map((item) => (
+              <li
+                key={item}
+                className="hover:text-yellow-400 transition-colors duration-300 text-base"
               >
-                {item}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className="text-white flex gap-4">
-          {mobileNavItems.map((item) => (
-            <li
-              key={item.name}
-              className="list-none hover:text-yellow-400 transition-colors duration-300"
-            >
-              <a
-                href={item.link}
-                target={item.target}
-                rel="noopener noreferrer"
+                <Link
+                  to={item.toLowerCase()}
+                  smooth={true}
+                  duration={500}
+                  className="cursor-pointer"
+                >
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className="text-white flex gap-4">
+            {mobileNavItems.map((item) => (
+              <li
+                key={item.name}
+                className="list-none hover:text-yellow-400 transition-colors duration-300"
               >
-                <item.icon className="inline-block w-6 h-6 mr-2" />
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+                <a
+                  href={item.link}
+                  target={item.target}
+                  rel="noopener noreferrer"
+                >
+                  {/* change icon color */}
+                  <item.icon className="inline-block w-6 h-6 mr-2 text-yellow-400" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
     </section>
   );
 };

a9eb7f3405f682cc96b486f32c19a95fd00a3485
ajacho <belen.jacho18@gmail.com>
2026-04-24 14:43:17 -0700

    testing bg in sm screen

--- src/components/sections/Landing.jsx
+++ src/components/sections/Landing.jsx
@@ -6,16 +6,18 @@ export const Landing = () => {
     <section
       id="land
       ing"
-      className="min-h-screen flex items-top justify-center text-[#2b3f55]
-      bg-[url('/public/llama_bg.png')] bg-cover bg-center relative bg-fixed
-      "
+      className="min-h-screen flex items-start justify-center text-[#2b3f55]
+  bg-[url('/llama_bg.png')] bg-cover bg-left-top bg-no-repeat
+  md:bg-center md:bg-fixed
+  relative
+"
     >
-      <div className="max-w-4xl mx-auto px-6 pt-20 text-justify">
+      <div className="max-w-4xl mx-auto px-6 pt-30 ">
         <h2 className="text-2xl text-[#54451b] ">Hi there, I'm</h2>
         <br />
         <h1 className="text-4xl top-100 font-bold text-size-xs">
           <TypeAnimation
-            sequence={["Andrea :)", 1000]}
+            sequence={["Andrea", 1000]}
             speed={{ type: "keyStrokeDelayInMs", value: 250 }}
             style={{ fontSize: "2em" }}
             repeat={Infinity}
